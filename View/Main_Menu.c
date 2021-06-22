a64111e54ae93598ecc17b1d3e7433585c6e224f
binxxi <1551097460@qq.com>
2021-06-22 23:57:27 +0800

    main_menu

--- View/Main_Menu.c
+++ View/Main_Menu.c
@@ -17,153 +17,372 @@
 
 #include "SalesAnalysis_UI.h"
 
+#include "Returnticket_UI.h"
+#include "Account_UI.h"
 //系统主菜单 
-void Main_Menu_Adinm(void) {
-	char choice;
-	do { 
-		printf("\n==================================================================\n");
-		printf("**************************** 河西村剧院管理系统****************************\n");
-		printf("[S] 管理演出厅\n");
-		printf("[A] 账户管理\n");
-		printf("[E] 退出\n");
-		printf("\n==================================================================\n");
-		printf("请输入你的选择:");
-		fflush(stdin);
-		choice = getchar();
-		switch (choice) {
-		case 'S':
-		case 's':
-			Studio_UI_MgtEntry();
-			break;
-		case 'A':
-		case 'a':
-			Account_UI_MgtEntry();
-			break;
-		}
-	} while ('E' != choice && 'e' != choice);
+void Main_Menu_Adinm(void)
+{
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n");
+    color(10); printf("                                              河西村剧院\n\n\n");
+    color(12); printf("                                              管理演出厅\n");
+    color(7);  printf("                                              管理账户\n");
+    color(7);  printf("                                              退出\n");
+    int ch, a = 1;
+    while (1)
+    {
+        ch = getch();
+        system("cls");
+        if (ch == 80)
+            a++;
+        if (a > 3) a = 3;
+        if (ch == 72)
+            a--;
+        if (a < 1) a = 1;
+        printf("\n\n\n\n\n\n\n\n");
+        color(10); printf("                                              河西村剧院\n\n");
+        if (a == 1)
+        {
+            color(12); printf("                                               管理演出厅\n");
+        }
+        else
+        {
+            color(7);  printf("                                               管理演出厅\n");
+        }
+        if (a == 2) {
+            color(12); printf("                                               管理账户\n");
+        }
+        else
+        {
+            color(7);  printf("                                               管理账户\n");
+        }
+        
+        if (a == 3)
+        {
+            color(12); printf("                                               退出\n");
+        }
+        else
+        {
+            color(7);  printf("                                               退出\n");
+        }
+        if (ch == 13)
+        {
+            if (a == 1)
+            {
+                Studio_UI_MgtEntry();
+                system("cls");
+                printf("\n\n\n\n\n\n\n\n");
+                color(10); printf("                                              河西村剧院\n\n");
+                color(12); printf("                                              管理演出厅\n");
+                color(7);  printf("                                              管理账户\n");
+                color(7);  printf("                                              退出\n");
+            }
+            if (a == 2)
+            {
+                Account_UI_MgtEntry();
+                system("cls");
+                printf("\n\n\n\n\n\n\n\n");
+                color(10); printf("                                              河西村剧院\n");
+                color(7);  printf("                                              管理演出厅\n");
+                color(12); printf("                                              管理账户\n");
+                color(7);  printf("                                              退出\n");
+            }
+            if (a == 3)
+            {
+                
+                system("cls");
+				    printf("\n\n\n\n\n\n\n\n");
+				    color(10); printf("                                            河西村剧院\n\n");
+				    color(12); printf("                                               登录\n");
+				    color(7);  printf("                                               退出\n");
+				    return;
+            }
+        }
+        if (ch == 27)
+        {
+        	system("cls");
+			    printf("\n\n\n\n\n\n\n\n");
+			    color(10); printf("                                            河西村剧院\n\n");
+			    color(12); printf("                                               登录\n");
+			    color(7);  printf("                                               退出\n");
+            return;
+        }
+    }
 }
-void Main_Menu_manger(void) {
-	char choice;
-	do { 
-		printf("\n==================================================================\n");
-		printf("**************************** 河西村剧院管理系统****************************\n");
-		//printf("[S] 管理演出厅\n");
-		printf("[P] 管理剧目\n");
-		//printf("[T] 售票\n");
-		//printf("[R] 退票\n");
-		printf("[Q] 查询\n");
-		printf("[N] 票房统计\n");
-		printf("[D] 统计销售额\n");
-		//printf("[A] 账户管理\n");
-		printf("[E] 退出\n");
-		printf("\n==================================================================\n");
-		printf("请输入你的选择:");
-		fflush(stdin);
-		choice = getchar();
-		switch (choice) {
-		//case 'S':
-		//case 's':
-			//Studio_UI_MgtEntry();
-			//break;
-		case 'P':
-		case 'p':
-			Play_UI_MgtEntry(0);
-			break;
-		case 'Q':
-		case 'q':
-			Queries_Menu();
-			break;
-		case 'd':
-		case 'D':
-			StaSales_UI_MgtEntry();
-			break;
-			
-		//case 'T':
-		//case 't':
-			//Sale_UI_MgtEntry();
-			//break;
-/*
-		case 'R':
-		case 'r':
-			Sale_UI_ReturnTicket();
-			break;*/
-		case 'N':
-		case 'n':
-			SalesAnalysis_UI_MgtEntry();
-			break;
+void Main_Menu_manger(void)
+{
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n");
+    color(10); printf("                                            河西村剧院\n\n");
+    color(12); printf("                                               管理剧目\n");
+    color(7);  printf("                                               查询\n");
+    color(7);  printf("                                               统计票房\n");
+    color(7);  printf("                                               统计销售额\n");
+    color(7);  printf("                                               退出\n");
+    int ch, a = 1;
+    while (1)
+    {
+        ch = getch();
+        system("cls");
+        if (ch == 80)
+            a++;
+        if (a > 5) a = 5;
+        if (ch == 72)
+            a--;
+        if (a < 1) a = 1;
+        printf("\n\n\n\n\n\n\n\n");
+        color(10); printf("                                            河西村剧院\n\n");
+        if (a == 1)
+        {
+            color(12); printf("                                               管理剧目\n");
+        }
+        else
+        {
+            color(7);  printf("                                               管理剧目\n");
+        }
+        if (a == 2) {
+            color(12); printf("                                               查询\n");
+        }
+        else
+        {
+            color(7);  printf("                                               查询\n");
+        }
+        if (a == 3)
+        {
+            color(12); printf("                                               统计票房\n");
+        }
+        else
+        {
+            color(7);  printf("                                               统计票房\n");
+        }
+        if (a == 4)
+        {
+            color(12); printf("                                               统计销售额\n");
+        }
+        else
+        {
+            color(7);  printf("                                               统计销售额\n");
+        }
+        if (a == 5)
+        {
+            color(12); printf("                                               退出\n");
+        }
+        else
+        {
+            color(7);  printf("                                               退出\n");
+        }
+        if (ch == 13)
+        {
+            if (a == 1)
+            {
+                Play_UI_MgtEntry(0);
+                system("cls");
+				    printf("\n\n\n\n\n\n\n\n");
+				    color(10); printf("                                            河西村剧院\n\n");
+				    color(12); printf("                                               管理剧目\n");
+				    color(7); printf("                                               查询\n");
+				    color(7); printf("                                               统计票房\n");
+				    color(7); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 2)
+            {
+                Queries_Menu();
+                system("cls");
+				    printf("\n\n\n\n\n\n\n\n");
+				    color(10); printf("                                            河西村剧院\n\n");
+				    color(7); printf("                                               管理剧目\n");
+				    color(12); printf("                                               查询\n");
+				    color(7); printf("                                               统计票房\n");
+				    color(7); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 3)
+            {
+                SalesAnalysis_UI_MgtEntry();
+                system("cls");
+				    printf("\n\n\n\n\n\n\n\n");
+				    color(10); printf("                                            河西村剧院\n\n");
+				    color(7); printf("                                               管理剧目\n");
+				    color(7); printf("                                               查询\n");
+				    color(12); printf("                                               统计票房\n");
+				    color(7); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 4)
+            {
+                StaSales_UI_MgtEntry();
+                system("cls");
+				    printf("\n\n\n\n\n\n\n\n");
+				    color(10); printf("                                            河西村剧院\n\n");
+				    color(7); printf("                                               管理剧目\n");
+				    color(7); printf("                                               查询\n");
+				    color(7); printf("                                               统计票房\n");
+				    color(12); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 5)
+            {
+            	system("cls");
+				printf("\n\n\n\n\n\n\n\n");
+				color(10); printf("                                            河西村剧院\n\n");
+				color(12); printf("                                               登录\n");
+				color(7);  printf("                                               退出\n");
+                return;
+            }
+        }
+        if (ch == 27)
+        {
+        	system("cls");
+			    printf("\n\n\n\n\n\n\n\n");
+			    color(10); printf("                                            河西村剧院\n\n");
+			    color(12); printf("                                               登录\n");
+			    color(7); printf("                                               退出\n");
+            return;
+            
+        }
+    }
+}
+void Main_Menu_Sale(void)
+{
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n");
+    color(10); printf("                                            河西村剧院\n\n");
+    color(12); printf("                                               售票\n");
+    color(7); printf("                                               退票\n");
+    color(7); printf("                                               查询\n");
+    color(7); printf("                                               统计销售额\n");
+    color(7); printf("                                               退出\n");
+    int ch, a = 1;
+    while (1)
+    {
+        ch = getch();
+        system("cls");
+        if (ch == 80)
+            a++;
+        if (a > 5) a = 5;
+        if (ch == 72)
+            a--;
+        if (a < 1) a = 1;
+        printf("\n\n\n\n\n\n\n\n");
+        color(10); printf("                                            河西村剧院\n\n");
+        if (a == 1)
+        {
+            color(12); printf("                                               售票\n");
+        }
+        else
+        {
+            color(7); printf("                                               售票\n");
+        }
+        if (a == 2) {
+            color(12); printf("                                               退票\n");
+        }
+        else
+        {
+            color(7); printf("                                               退票\n");
+        }
+        if (a == 3)
+        {
+            color(12); printf("                                               查询\n");
+        }
+        else
+        {
+            color(7); printf("                                               查询\n");
+        }
+        if (a == 4)
+        {
+            color(12); printf("                                               统计销售额\n");
+        }
+        else
+        {
+            color(7); printf("                                               统计销售额\n");
+        }
+        if (a == 5)
+        {
+            color(12); printf("                                               退出\n");
+        }
+        else
+        {
+            color(7); printf("                                               退出\n");
+        }
+        if (ch == 13)
+        {
+            if (a == 1)
+            {
+                Sale_UI_MgtEntry();
+                system("cls");printf("\n\n\n\n\n\n\n\n");
+                color(10); printf("                                            河西村剧院\n\n");
+				    color(12); printf("                                               售票\n");
+				    color(7); printf("                                               退票\n");
+				    color(7); printf("                                               查询\n");
+				    color(7); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 2)
+            {
+                Sale_UI_RetfundTicket();
+                 system("cls");printf("\n\n\n\n\n\n\n\n");
+                color(10); printf("                                            河西村剧院\n\n");
+				    color(7); printf("                                               售票\n");
+				    color(12); printf("                                               退票\n");
+				    color(7); printf("                                               查询\n");
+				    color(7); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 3)
+            {
+                Queries_Menu();
+                 system("cls");printf("\n\n\n\n\n\n\n\n");
+                color(10); printf("                                            河西村剧院\n\n");
+				    color(7); printf("                                               售票\n");
+				    color(7); printf("                                               退票\n");
+				    color(12); printf("                                               查询\n");
+				    color(7); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 4)
+            {
+                StaSales_UI_MgtEntry();
+                 system("cls");printf("\n\n\n\n\n\n\n\n");
+                color(10); printf("                                            河西村剧院\n\n");
+				    color(7); printf("                                               售票\n");
+				    color(7); printf("                                               退票\n");
+				    color(7); printf("                                               查询\n");
+				    color(12); printf("                                               统计销售额\n");
+				    color(7); printf("                                               退出\n");
+            }
+            if (a == 5)
+            {
+            	system("cls");
+				printf("\n\n\n\n\n\n\n\n");
+				color(10); printf("                                            河西村剧院\n\n");
+				color(12); printf("                                               登录\n");
+				color(7);  printf("                                               退出\n");
+                return;
+            }
+        }
+        if (ch == 27)
+        {
+        	system("cls");
+			    printf("\n\n\n\n\n\n\n\n");
+			    color(10); printf("                                            河西村剧院\n\n");
+			    color(12); printf("                                               登录\n");
+			    color(7); printf("                                               退出\n");
+            return;
+        }
+    }
+}
 
-/*	    case 'A':
-		case 'a':
-			Account_UI_MgtEntry();
-			break;*/
-		}
-	} while ('E' != choice && 'e' != choice);
-}
-void Main_Menu_Sale(void) {
-	char choice;
-	do { 
-		printf("\n==================================================================\n");
-		printf("**************************** 河西村剧院管理系统****************************\n");
-		//printf("[S] 管理演出厅\n");
-		//printf("[P] 管理剧目\n");
-		printf("[T] 售票\n");
-		printf("[R] 退票\n");
-		printf("[Q] 查询\n");
-		//printf("[N] 票房统计\n");
-		printf("[D] 统计销售额\n");
-		//printf("[A] 账户管理\n");
-		printf("[E] 退出\n");
-		printf("\n==================================================================\n");
-		printf("请输入你的选择:");
-		fflush(stdin);
-		choice = getchar();
-		switch (choice) {
-	/*	case 'S':
-		case 's':
-			Studio_UI_MgtEntry();
-			break;
-		case 'P':
-		case 'p':
-			Play_UI_MgtEntry(0);
-			break;*/
-		case 'd':
-		case 'D':
-			StaSales_UI_MgtEntry();
-			break;
-		case 'Q':
-		case 'q':
-			Queries_Menu();
-			break;
-		case 'T':
-		case 't':
-			Sale_UI_MgtEntry();
-			break;
-/*
-		case 'R':
-		case 'r':
-			Sale_UI_ReturnTicket();
-			break;
-		case 'N':
-		case 'n':
-			SalesAanalysis_UI_MgtEntry();
-			break;
-*/
-	/*	case 'A':
-		case 'a':
-			Account_UI_MgtEntry();
-			break;*/
-		}
-	} while ('E' != choice && 'e' != choice);
-}
 
 void Queries_Menu()
 {
 	system("cls");
-	printf("\n\n\n\n\n------------------------------------\n");
+	printf("=====================================\n");
 	printf("1.查询演出\n");
 	printf("2.查询演出票\n");
 	printf("3.退出\n");
-	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("=====================================\n");
+	printf("请输入你的选择：");
 	int choice;
 	scanf("%d",&choice);
 	switch(choice)
@@ -174,7 +393,7 @@ void Queries_Menu()
 			break;
 		case 3:return ;
 		default :
-			printf("\n\n\n输入有误");
+			printf("输入有误\n");
 			system("pause");
 	}
 }
