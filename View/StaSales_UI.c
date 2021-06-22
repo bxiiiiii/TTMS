dcb7cedf2296f12824d4b2d01156a091062f2634
binxxi <1551097460@qq.com>
2021-06-22 23:58:59 +0800

    stasales

--- View/StaSales_UI.c
+++ View/StaSales_UI.c
@@ -1,9 +1,8 @@
 #include "StaSales_UI.h"
-
+#include "Play_UI.h"
 #include "../Common/List.h"
 #include "../Service/Sale.h"
 #include "../Service/Account.h"
-#include "../Service/StaSales.h"
 #include <stdio.h>
 #include <time.h>
 
@@ -36,35 +35,22 @@ void StaSales_UI_Self(){
     int id;
     id=gl_CurUser.id;
 
-    ttms_date_t curdate;
-    ttms_date_t startdate;
-    ttms_date_t enddate;
+    user_date_t curdate;
+    //ttms_date_t startdate;
+    //ttms_date_t enddate;
 
     struct tm *local;
     time_t t;
-    t=time(&t);
-    local=localtime(&t); 
     
-    curdate.year=local->tm_year+1900;
-    curdate.month=local->tm_mon+1;
-    curdate.day=local->tm_mday;
-
-    startdate.year=local->tm_year+1900;
-    startdate.month=local->tm_mon+1;
-    startdate.day=1;
-
-    startdate.year=local->tm_year+1900;
-    startdate.month=local->tm_mon+1;
-    startdate.day=31;
+    curdate = DateNow();
 
     int choice;
     do
     {
-        system("cls");
+    	system("cls");
     printf("=======================================\n");
-    printf("[1]当日销售额 | [2]当月销售额 | [0]返回\n");
+    printf(" [1]当日销售额 \n [2]当月销售额 \n [0]返回\n");
     printf("---------------------------------------\n");
-
     printf("请选择你想查询的选项：");
     setbuf(stdin,NULL);
     scanf("%d",&choice);
@@ -76,7 +62,7 @@ void StaSales_UI_Self(){
     {
         case 1: 
             system("cls");
-            count=StaSales_Srv_CompSaleVal(id,curdate,curdate);
+            count=Salesanalysis_Srv_CompSaleVal(id,curdate,1);
             printf("===========================================\n");
             printf("%5s  %5s  %5s","ID","类型","姓名\n");
             printf("%5d  %5d  %5s\n",gl_CurUser.id,gl_CurUser.type,gl_CurUser.username);
@@ -85,7 +71,7 @@ void StaSales_UI_Self(){
             break;
         case 2:
             system("cls");
-            count=StaSales_Srv_CompSaleVal(id,startdate,enddate);
+            count=Salesanalysis_Srv_CompSaleVal(id,curdate,2);
             printf("===========================================\n");
             printf("%5s  %5s  %5s","ID","类型","姓名\n");
             printf("%5d  %5d  %5s\n",gl_CurUser.id,gl_CurUser.type,gl_CurUser.username);
@@ -98,50 +84,43 @@ void StaSales_UI_Self(){
 }
 
 
+
 void StaSales_UI_Clerk()
 {
     int id;
     char username[100];
     ttms_date_t startdate;
     ttms_date_t enddate;
-    
-    account_list_t list;
-    List_Init(list, account_list_t);
-    
     system("cls");
     printf("请输入售票员姓名：");
     setbuf(stdin,NULL);
     scanf("%s",username);
     setbuf(stdin,NULL);
     system("cls");
-	account_list_t pos;
+
     account_t user;//存储用户信息
     int i;
-    //i=Account_Srv_FetchByName(username,&user);
-    if(Account_Srv_FetchAll(list))
-    {
-		//获取成功 
-	}
-    pos = Account_Srv_FindByUsrName(list, username);
-	id=pos->data.id;
+    i=Account_Srv_FetchByName(username,&user);
+    id=user.id;
     if(i==0)
     {
         printf("该用户不存在！");
         return;
     }
-    printf("请输入要查询的开始日期(x/y/z):");
+    printf("请输入要查询的开始日期(x/y/z):\n");
     setbuf(stdin,NULL);
     scanf("%d/%d/%d",&startdate.year,&startdate.month,&startdate.day);
     setbuf(stdin,NULL);
-    printf("输入要查询的结束日期(x/y/z)：");
+    printf("输入要查询的结束日期(x/y/z)\n");
     setbuf(stdin,NULL);
     scanf("%d/%d/%d",&enddate.year,&enddate.month,&enddate.day);
     int count;
-    count=StaSales_Srv_CompSaleVal(id,startdate,enddate);
+    count=Salesanalysis_Srv_CompSaleVal_1(id,startdate,enddate);
+	
     printf("\n\n\n\n");
     printf("===========================================\n");
     printf("%5s  %5s  %5s","ID","类型","姓名\n");
-    printf("%5d  %5d  %5s\n",pos->data.id,pos->data.type,pos->data.username);
+    printf("%5d  %5d  %5s\n",user.id,user.type,user.username);
     printf("销售总额为：%5d\n",count);
     printf("*******************************************\n");
     system("pause");
